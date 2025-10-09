436434257822211322eb1d67983360d9844ea8e3
Vikramnarasimhan <vikram.narasimhan@psiog.com>
2025-10-09 15:18:00 +0530

    starting with recursion algos

---
+++ recursionalgos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Collections.Generic;
+class Recursion {
+    int factorial(n) {
+        if (n == 0) {
+            return 1;
+        
+        }
+        return n * factorial(n-1);
+
+        
+    
+    
+    }
+    static void Main()
+    {
+
+        Console.WriteLine($"{factorial(5)}");
+
+    }
+
+
+
+}
