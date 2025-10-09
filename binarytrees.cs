dd1d7171b28ec6d0d8312298c1cc96ebc6da4ef5
Vikramnarasimhan <vikram.narasimhan@psiog.com>
2025-10-09 11:46:45 +0530

    binary tree created

---
+++ binarytrees.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Collections.Generic;
+public class BinaryTree {
+    public class Node {
+        public int Data;
+        public Node Left;
+        public Node Right;
+        public Node(int data) {
+            Data = data;
+            Left = null;
+            Right = null;
+
+        
+        }
+    }
+    private Node root;
+    public BinaryTree() { 
+    root=null;
+    
+    }
+    public void insert(int data) {
+        root = insertrecord(root,data);
+    
+    }
+    private Node insertrecord(Node current,int data) {
+        if (current == null) {
+            return new Node(data);
+        
+        }
+        if (current.Data > data)
+        {
+           current.Left= insertrecord(current.Left, data);
+        }
+        else {
+
+            current.Right= insertrecord(current.Right, data);
+        }
+        return current;
+
+    
+    
+    }
+
+
+
+
+
+}
