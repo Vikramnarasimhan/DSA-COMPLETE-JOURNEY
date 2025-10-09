64659c35264c3cc808ceb584d7d5c015ce4337da
Vikramnarasimhan <vikram.narasimhan@psiog.com>
2025-10-09 14:56:13 +0530

    performed stack operations

---
+++ stacks.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Collections.Generic;
+class Stacks {
+    static void Main() {
+        Stack<int> stacks = new Stack<int>();
+        stacks.Push(1); 
+        stacks.Push(2); 
+        stacks.Push(3);
+        Console.WriteLine(stacks.Count);
+        stacks.Pop();
+        Console.WriteLine(stacks.Count);
+        stacks.Push(4); 
+        stacks.Push(5);
+      
+
+    
+    }
+
+
+
+}
