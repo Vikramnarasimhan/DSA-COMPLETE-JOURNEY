a500f876734aa04aafbbdddfcb1d32e08ccde468
Vikramnarasimhan <vikram.narasimhan@psiog.com>
2025-10-10 11:21:58 +0530

    finish implementation of three types of graphs

---
+++ graphs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+class Graphs {
+    private Dictionary<string , List<string>> adjlist;
+    public Graphs() { 
+
+    
+        adjlist=new Dictionary<string, List<string>>(); 
+    }
+    public void addvertex(string vertex) {
+        if (!adjlist.ContainsKey(vertex)) {
+            adjlist[vertex] = new List<string>();
+
+        }
+    }
+    public void addedge(string from,string to) {
+        addvertex(from); addvertex(to); 
+        adjlist[from].Add(to);  
+    }
+    public void PrintGraph() {
+        foreach (var items in adjlist) {
+            Console.Write(items.Key+"->");
+            Console.WriteLine(string.Join(",",items.Values));
+        
+        
+        
+        }
+    
+    
+    
+    }
+
+
+
+
+}
