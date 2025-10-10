77069c8a6dd322d183fbe596b87dafd793b07427
Vikramnarasimhan <vikram.narasimhan@psiog.com>
2025-10-10 14:11:01 +0530

    finishing up arrays

--- Arrays.cs
+++ Arrays.cs
@@ -66,6 +66,22 @@ class Arrays {
             
         
         }
+        // Creating a jagged array
+        //int[][] jagged = new int[3][];
+        //jagged[0] = new int[] { 1, 2 };
+        //jagged[1] = new int[] { 3, 4, 5 };
+        //jagged[2] = new int[] { 6 };
+
+        //// Iterating through it
+        //for (int i = 0; i < jagged.length; i++)
+        //{
+        //    for (int j = 0; j < jagged[i].length; j++)
+        //    {
+        //        System.out.print(jagged[i][j] + " ");
+        //    }
+        //    System.out.println();
+        //}
+
 
 
 
